7ccea99aae6d9f2f271cde53da21f8974d4f91f8
Mark Muthii <mark.muthii@strathmore.edu>
2023-11-03 21:45:42 +0300

    into the light

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+const emailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GOOGLE_APP_USER,
+    pass: process.env.GOOGLE_APP_PASSWORD,
+  },
+});
+
+emailTransport.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const { firstName, lastName, email, message, phone } = req.body;
+  const mail = {
+    from: `${firstName} ${lastName} <${email}>`,
+    to: process.env.GOOGLE_APP_USER,
+    replyTo: `${firstName} ${lastName} <${email}>`,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${firstName} ${lastName}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  emailTransport.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+
+app.listen(5000, () => console.log("Server Running"));
